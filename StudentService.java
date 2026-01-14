8d06a4f8e51d23811f4d8c6a8d14f26ae599c881
malavikag-101 <malavikag101@gmail.com>
2026-01-14 14:54:53 +0530

    Create StudentService.java

---
+++ StudentService.java
@@ -0,0 +1,32 @@
+package com.example.studentmanagement.service;
+
+import java.util.List;
+import org.springframework.stereotype.Service;
+import com.example.studentmanagement.entity.Student;
+import com.example.studentmanagement.repository.StudentRepository;
+
+@Service
+public class StudentService {
+
+    private final StudentRepository repo;
+
+    public StudentService(StudentRepository repo) {
+        this.repo = repo;
+    }
+
+    public List<Student> getAllStudents() {
+        return repo.findAll();
+    }
+
+    public Student saveStudent(Student student) {
+        return repo.save(student);
+    }
+
+    public Student getStudentById(Long id) {
+        return repo.findById(id).orElse(null);
+    }
+
+    public void deleteStudent(Long id) {
+        repo.deleteById(id);
+    }
+}
