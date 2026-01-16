606e2aa08baa971a9613d5593724594aaf0d14a5
malavikag-101 <malavikag101@gmail.com>
2026-01-16 21:03:30 +0530

    Update StudentController.java

--- StudentController.java
+++ StudentController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.*;
 import com.example.studentmanagement.entity.Student;
 import com.example.studentmanagement.service.StudentService;
 
-@Controller
+
 public class StudentController {
 
     private final StudentService service;
@@ -15,32 +15,32 @@ public class StudentController {
         this.service = service;
     }
 
-    @GetMapping("/")
+    
     public String viewHome(Model model) {
         model.addAttribute("students", service.getAllStudents());
         return "list";
     }
 
-    @GetMapping("/add")
+    
     public String addStudentForm(Model model) {
         model.addAttribute("student", new Student());
         return "add";
     }
 
-    @PostMapping("/save")
-    public String saveStudent(@ModelAttribute Student student) {
+    
+    public String saveStudent( Student student) {
         service.saveStudent(student);
         return "redirect:/";
     }
 
-    @GetMapping("/edit/{id}")
-    public String editStudent(@PathVariable Long id, Model model) {
+    
+    public String editStudent( Long id, Model model) {
         model.addAttribute("student", service.getStudentById(id));
         return "add";
     }
 
-    @GetMapping("/delete/{id}")
-    public String deleteStudent(@PathVariable Long id) {
+    
+    public String deleteStudent( Long id) {
         service.deleteStudent(id);
         return "redirect:/";
     }
