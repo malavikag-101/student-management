bd20559b41ffccc00829260e5b3d2aaa125ca4ad
malavikag-101 <malavikag101@gmail.com>
2026-01-16 21:03:55 +0530

    Update StudentManagementApplication.java

--- StudentManagementApplication.java
+++ StudentManagementApplication.java
@@ -3,7 +3,7 @@ package com.example.studentmanagement;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+
 public class StudentManagementApplication {
 
     public static void main(String[] args) {
